74e1b7cf88701e8034516a0e51598282a679c2b3
August "Agpvpz <87880073+Agpvpz@users.noreply.github.com>
2024-10-28 13:27:19 +0100

    idk bro

--- scripts/supabase.gd
+++ scripts/supabase.gd
@@ -1,15 +1,10 @@
 extends Node
 
 class_name Supabase
-
-<<<<<<< HEAD
 # Supabase configuration (Set your own Supabase URL and API Key)
 var supabase_url: String = "https://nnsqtgnakefibqsakdwv.supabase.co/rest/v1/highscores"
 var supabase_key: String = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5uc3F0Z25ha2VmaWJxc2FrZHd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ4NDA5NzEsImV4cCI6MjA0MDQxNjk3MX0.JqnXYZ8CaPIoWG4S4Bj3Mi3VOfZTrSvNx_I_A4HaF0k"
-=======
-var supabase_url: String = "https://your-project.supabase.co"
-var supabase_key: String = "your-supabase-key"
->>>>>>> 9891a4039f49925ba04c683908172be6d4f6a8be
+
 
 var http: HTTPRequest = null
 var json: JSON = null
